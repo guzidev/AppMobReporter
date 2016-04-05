b35f8ea82179db3f974a484a75e39ba0048ac786
guzi job <guzijob@gmail.com>
2016-04-05 14:55:35 +0200

    connect controllers to new API

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,10 +1,26 @@
 angular.module('citizen-engagement.controllers', [])
 
-        .controller('UserController', function($scope) {
+        .controller('UserController', function($scope, $http) {
            $scope.data = [
 			{ firstName: "John", lastName: "Doe" },
 			{ firstName: "John", lastName: "Smith" },
 			{ firstName: "David", lastName: "Lowe" },
 			{ firstName: "Mark", lastName: "Holloway" }
 		]; 
+                
+            //$http.get('apiUrl')
 })
+
+.controller('IssuesCtrl', function($scope, $http, apiUrl) {
+    $scope.issues = {};
+    $http({
+        method: 'GET',
+        url: apiUrl + '/issues',
+        headers: {
+            'x-pagination': '0;100'
+        }
+    })
+    .success(function(issues) {
+        $scope.issues = issues;
+    });
+});
