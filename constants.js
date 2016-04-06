f267689beab3e319980ad29207dc94b4329be866
Kamsmm42 <kemal.evans@heig-vd.ch>
2016-04-06 14:34:26 +0200

    logo

--- constants.js
+++ constants.js
@@ -1,4 +1,6 @@
 angular.module('citizen-engagement.constants', [])
   .constant('apiUrl', '@apiUrl@')
+  .constant('mapboxMapId', 'guzidev.pk0agp0l')
+   .constant('mapboxAccessToken', 'pk.eyJ1IjoiZ3V6aWRldiIsImEiOiJjaW1vcXhzYnYwMDFkd2FtNHJxeDd6cWVzIn0.wvnNee-JBfVrwEWF_gVePA')
   // above url correct?
 ;
