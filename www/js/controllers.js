7d182379733bb4f2fab8be146c1baec631886f3a
guzi job <guzijob@gmail.com>
2016-04-06 11:35:13 +0200

    update issueController with filter method

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -22,11 +22,59 @@ angular.module('citizen-engagement.controllers', [])
 
 .controller('IssuesCtrl', function($scope, $http, apiUrl) {
     $scope.issues = {};
+    
     $http({
         method: 'GET',
         url: apiUrl + '/issues',
         headers: {
-            'x-pagination': '0;20'
+            'x-pagination': '0;20',          
+        }
+    })
+    .success(function(issues) {
+        $scope.issues = issues;
+    });
+
+    $scope.filterIssues = function() {
+
+        // Show a loading message if the request takes too long.
+        $ionicLoading.show({
+        template: 'Logging in...',
+        delay: 750
+        });
+
+        $http({
+            method: 'GET',
+            url: apiUrl + '/issues',
+            headers: {
+            'Content-Type': 'application/json',
+            'x-pagination': '0;20',          
+            },
+            data: {
+                //XX
+            }
+    }).then(
+        function(issues) {
+            $ionicLoading.hide();
+            // necessary? // Go to the issue list tab.
+            //$state.go('tab.myIssues');
+        },
+        function() {
+            // If an error occurs, hide the loading message and show an error message.
+            $ionicLoading.hide();
+            $scope.error = 'Could not get any Issues.';
+      }
+    )
+    }
+})
+
+.controller('MyIssuesCtrl', function($scope, $http, apiUrl) {
+    $scope.issues = {};
+    $http({
+        method: 'GET',
+        url: apiUrl + '/me/issues',
+        headers: {
+            'x-pagination': '0;20',
+            'x-user-id': AuthService.currentUserId
         }
     })
     .success(function(issues) {
